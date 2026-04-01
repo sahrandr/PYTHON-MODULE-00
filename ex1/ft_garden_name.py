913f12f8bcdb82e5fb845de2aa40fe865432859c
Sahala Mayah Randriamamonjy <sahrandr@student.42antananarivo.mg>
2026-04-01 04:52:55 +0300

    new

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_name.py                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: sahrandr <sahrandr@student.42antananari    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/03/29 16:19:08 by sahrandr          #+#    #+#              #
+#    Updated: 2026/03/29 16:19:16 by sahrandr         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def ft_garden_name():
+    name = input("Enter garden name: ")
+    print("Garden:", name)
+    print("Status: Growing well!")
